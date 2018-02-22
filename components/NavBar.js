ac88374801c824956b515315fab1ac86102703b2
Peng Jie <bivinity.pengzjie@gmail.com>
2018-02-22 16:01:20 +0800

    Update layout

---
+++ components/NavBar.js
@@ -0,0 +1,7 @@
+export default () => (
+  <nav className="navbar navbar-dark bg-dark" style={{ marginBottom: '50px' }}>
+    <a className="navbar-brand" href="#">
+      591 租屋簡易查詢
+    </a>
+  </nav>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from '../components/Head';
+import NavBar from '../components/NavBar';
 import App from '../components/App';
 
 export default () => (
   <div>
     <Head />
+    <NavBar />
     <App />
   </div>
 );
