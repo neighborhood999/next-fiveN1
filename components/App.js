661cafdfcef2636495bf98f71f431386df5ee0f3
Peng Jie <bivinity.pengzjie@gmail.com>
2018-03-15 21:29:27 +0800

    rough layout

--- components/App.js
+++ components/App.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import QueryForm from './QueryForm';
+import RentInfoList from './RentInfoList';
 
-class App extends Component {
-  state = { data: {} };
+class App extends PureComponent {
+  state = { data: [] };
 
-  updateFetchResponse = data => {
-    this.setState(() => ({ data }));
-  };
+  updateFetchResponse = rentInfoList =>
+    this.setState(state => ({
+      data: [...state.data, rentInfoList]
+    }));
 
   render() {
     return (
       <div className="container">
         <div className="row align-items-center">
-          <div className="col-md">
+          <div className="col-md-12">
             <QueryForm updateFetchResponse={this.updateFetchResponse} />
           </div>
+        </div>
+        <div className="row">
+          <RentInfoList data={this.state.data} />
         </div>
       </div>
     );
--- components/RentInfoList.js
+++ components/RentInfoList.js
@@ -1,3 +1,20 @@
+import styled from 'styled-components';
+import ReactTooltip from 'react-tooltip';
+import { Box, DollarSign, Grid, Hash, MapPin, Menu } from 'react-feather';
+
+const Title = styled.h5`
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`;
+
+const CardText = styled.p`
+  margin-top: 15px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`;
+
 const RentInfoList = ({ data }) => {
   const flattenData = [].concat(...data);
   const rentInfos = Object.entries(flattenData).map((rent, index) => (
@@ -5,20 +22,34 @@ const RentInfoList = ({ data }) => {
       <div className="card border-secondary mb-3">
         <img
           className="card-img-top"
-          src={rent[1].preview}
           alt="preview-photo"
+          src={rent[1].preview}
         />
         <div className="card-body">
-          <h5 className="card-title">{rent[1].title}</h5>
-          <p className="card-text">地址：{rent[1].address}</p>
-          <p className="card-text">價格：NT$ {rent[1].price}</p>
-          <p className="card-text">樓層：{rent[1].floor}</p>
-          <p className="card-text">類型：{rent[1].optionType}</p>
-          <p className="card-text">租屋類型：{rent[1].rentType}</p>
-          <p className="card-text">坪數：{rent[1].ping}</p>
-          <a className="btn btn-primary" href={rent[1].url}>
-            Detail
-          </a>
+          <Title className="card-title" data-tip={rent[1].title}>
+            {rent[1].title}
+          </Title>
+          <CardText className="card-text" data-tip={rent[1].address}>
+            <MapPin size={16} /> {rent[1].address}
+          </CardText>
+          <CardText className="card-text">
+            <DollarSign size={16} /> {rent[1].price}
+          </CardText>
+          <CardText className="card-text">
+            <Menu size={16} /> {rent[1].floor}
+          </CardText>
+          <CardText className="card-text">
+            <Box size={16} /> {rent[1].optionType}
+          </CardText>
+          <CardText className="card-text">
+            <Grid size={16} /> {rent[1].ping} 坪
+          </CardText>
+          <div className="float-right">
+            <a className="btn btn-primary" href={rent[1].url} target="_blank">
+              點此前往
+            </a>
+          </div>
+          <ReactTooltip />
         </div>
       </div>
     </div>
