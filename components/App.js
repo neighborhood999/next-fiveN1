02e9935889aa8de12e6c418a05be3b22d7e49924
Peng Jie <bivinity.pengzjie@gmail.com>
2018-03-17 21:10:36 +0800

    refactor with recompose

--- components/App.js
+++ components/App.js
@@ -1,29 +1,27 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+import { compose, withState, withHandlers } from 'recompose';
 import QueryForm from './QueryForm';
 import RentInfoList from './RentInfoList';
 
-class App extends PureComponent {
-  state = { data: [] };
+const enhance = compose(
+  withState('data', 'getRentInfoList', []),
+  withHandlers({
+    fetchResponse: ({ getRentInfoList }) => prevListState =>
+      getRentInfoList(nextList => [...prevListState, ...nextList])
+  })
+);
 
-  updateFetchResponse = rentInfoList =>
-    this.setState(state => ({
-      data: [...state.data, rentInfoList]
-    }));
-
-  render() {
-    return (
-      <div className="container">
-        <div className="row align-items-center">
-          <div className="col-md-12">
-            <QueryForm updateFetchResponse={this.updateFetchResponse} />
-          </div>
-        </div>
-        <div className="row">
-          <RentInfoList data={this.state.data} />
-        </div>
+const App = ({ data, fetchResponse }) => (
+  <div className="container">
+    <div className="row align-items-center">
+      <div className="col-md-12">
+        <QueryForm fetchResponse={fetchResponse} />
       </div>
-    );
-  }
-}
+    </div>
+    <div className="row">
+      <RentInfoList data={data} />
+    </div>
+  </div>
+);
 
-export default App;
+export default enhance(App);
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import NavBar from '../components/NavBar';
 import App from '../components/App';
 
 export default () => (
-  <div>
+  <div id="app">
     <Head />
     <NavBar />
     <App />
