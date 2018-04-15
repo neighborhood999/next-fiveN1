b822349c49d9aa6a38973f8d44f19d62597abe35
Peng Jie <bivinity.pengzjie@gmail.com>
2018-04-15 22:32:47 +0800

    feat: add `status` and `queryParameters` * refactor: update fetch more rent information * style: add fetch more button

--- components/App.js
+++ components/App.js
@@ -1,25 +1,72 @@
 import React from 'react';
+import fetch from 'isomorphic-unfetch';
 import { compose, withState, withHandlers } from 'recompose';
 import QueryForm from './QueryForm';
 import RentInfoList from './RentInfoList';
+import { appendParameters, handleResponse } from '../utils';
 
 const enhance = compose(
   withState('data', 'getRentInfoList', []),
+  withState('status', 'updateStatus', false),
+  withState('queryParameters', 'setQueryParameters', {}),
   withHandlers({
-    fetchResponse: ({ getRentInfoList }) => prevListState =>
-      getRentInfoList(nextList => [...prevListState, ...nextList])
+    fetchMore: ({
+      data,
+      queryParameters,
+      setQueryParameters,
+      getRentInfoList
+    }) => async () => {
+      const updateQueryParameters = {
+        ...queryParameters,
+        firstRow: parseInt(queryParameters.firstRow) + 30
+      };
+      const url = appendParameters(updateQueryParameters);
+      const nextPage = await fetch(url).then(response => response.json());
+      const [hasData, rentInfos] = handleResponse(nextPage);
+
+      if (hasData) {
+        getRentInfoList([...data, ...rentInfos]);
+        setQueryParameters(updateQueryParameters);
+      } else {
+        console.log('No more informations');
+      }
+    }
   })
 );
 
-const App = ({ data, fetchResponse }) => (
+const App = ({
+  data,
+  status,
+  getRentInfoList,
+  fetchMore,
+  updateStatus,
+  setQueryParameters
+}) => (
   <div className="container">
     <div className="row align-items-center">
       <div className="col-md-12">
-        <QueryForm fetchResponse={fetchResponse} />
+        <QueryForm
+          getRentInfoList={getRentInfoList}
+          updateStatus={updateStatus}
+          setQueryParameters={setQueryParameters}
+        />
       </div>
     </div>
     <div className="row">
       <RentInfoList data={data} />
+    </div>
+    <div className="row">
+      <div className="col-md-4 offset-md-4 pt-3 pb-3">
+        {status && (
+          <button
+            type="button"
+            className="btn btn-light btn-block"
+            onClick={fetchMore}
+          >
+            載入更多房屋資訊
+          </button>
+        )}
+      </div>
     </div>
   </div>
 );
