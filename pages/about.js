61a1b49d70811a0558c63ba17f88d5c3a6a4e584
Peng Jie <bivinity.pengzjie@gmail.com>
2018-05-10 10:54:36 +0800

    enhance: add ga to the page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,11 @@
+import Router from 'next/router';
 import Head from '../components/Head';
 import NavBar from '../components/NavBar';
 import About from '../components/About';
 import Footer from '../components/Footer';
+import { pageView } from '../utils';
+
+Router.onRouteChangeStart = url => pageView(url);
 
 export default () => (
   <div id="app">
