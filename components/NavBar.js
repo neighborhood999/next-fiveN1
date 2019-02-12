dff5f2c1a455d060840bd7659211ee928e4e253e
Peng Jie <dean.leehom@gmail.com>
2019-02-12 12:23:36 +0800

    refactor: use react hooks (#43)
    * refactor: use react hooks
    
    * refactor: drop recompose

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,54 +1,49 @@
+import { useState } from 'react';
 import Link from 'next/link';
-import { compose, withState, withHandlers } from 'recompose';
 
-const enhance = compose(
-  withState('collapseStatus', 'setCollapse', false),
-  withHandlers({
-    onClick: ({ setCollapse }) => event => {
-      setCollapse(status => !status);
-    }
-  })
-);
+function NavBar() {
+  const [collapseStatus, setCollapseStatus] = useState(false);
 
-const NavBar = ({ collapseStatus, onClick }) => (
-  <nav
-    className="navbar navbar-expand-lg navbar-dark bg-dark"
-    style={{ marginBottom: '50px' }}
-  >
-    <Link href="/">
-      <a className="navbar-brand">591 租屋簡易查詢</a>
-    </Link>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarToggler"
-      aria-controls="navbarToggler"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-      onClick={onClick}
+  return (
+    <nav
+      className="navbar navbar-expand-lg navbar-dark bg-dark"
+      style={{ marginBottom: '50px' }}
     >
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div
-      id="navbarToggler"
-      className={
-        collapseStatus
-          ? 'collapse navbar-collapse show'
-          : 'collapse navbar-collapse'
-      }
-    >
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link href="/about">
-            <a className="nav-link">
-              About <span className="sr-only">(current)</span>
-            </a>
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
-);
+      <Link href="/">
+        <a className="navbar-brand">591 租屋簡易查詢</a>
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarToggler"
+        aria-controls="navbarToggler"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        onClick={() => setCollapseStatus(status => !status)}
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div
+        id="navbarToggler"
+        className={
+          collapseStatus
+            ? 'collapse navbar-collapse show'
+            : 'collapse navbar-collapse'
+        }
+      >
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link href="/about">
+              <a className="nav-link">
+                About <span className="sr-only">(current)</span>
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
 
-export default enhance(NavBar);
+export default NavBar;
